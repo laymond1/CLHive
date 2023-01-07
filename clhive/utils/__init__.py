a93fb7ffb1b3b89824d10e6ce9a681cfb304a205
laymond1 <costor107@gmail.com>
2023-01-08 03:33:21 +0900

    📎 util functions added

--- clhive/utils/__init__.py
+++ clhive/utils/__init__.py
@@ -1,2 +1,2 @@
-from .generic import get_optimizer
+from .generic import seedEverything, get_optimizer, get_metrics
 from .console_display import ConsoleDisplay
--- clhive/utils/generic.py
+++ clhive/utils/generic.py
@@ -1,8 +1,25 @@
 from typing import Any, Callable, Dict, List, Optional
 from rich.console import Console
+from sklearn import metrics
+from scipy.optimize import brentq
+from scipy.interpolate import interp1d
 
+import numpy as np 
+import os 
+import random
 import torch
 
+
+DEFAULT_RANDOM_SEED = 2023
+
+def seedEverything(seed=DEFAULT_RANDOM_SEED):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 def get_optimizer(config: Dict[str, Any]):
     assert hasattr(
@@ -85,6 +102,34 @@ def save_(x, name="tmp.png"):
         x = x.reshape(-1, *x.shape[2:])
 
     save_image(x, name)
+
+
+# --- Representation utils
+def get_metrics(labels, scores, FPRs):
+    # eer and auc
+    fpr, tpr, _ = metrics.roc_curve(labels, scores, pos_label=1)
+    roc_curve = interp1d(fpr, tpr)
+    EER = 100. * brentq(lambda x : 1. - x - roc_curve(x), 0., 1.)
+    AUC = 100. * metrics.auc(fpr, tpr)
+
+    # get acc
+    tnr = 1. - fpr
+    # pos_num = labels.count(1)
+    pos_num = len(labels.nonzero()[0])
+    neg_num = len(labels) - pos_num
+    ACC = 100. * max(tpr * pos_num + tnr * neg_num) / len(labels)
+
+    # TPR @ FPR
+    if isinstance(FPRs, list):
+        TPRs = [
+            ('TPR@FPR={}'.format(FPR), 100. * roc_curve(float(FPR)))
+            for FPR in FPRs
+        ]
+    else:
+        TPRs = []
+
+    return [('ACC', ACC), ('EER', EER), ('AUC', AUC)] + TPRs
+
 
 
 # --- MIR utils
