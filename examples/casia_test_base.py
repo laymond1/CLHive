52b18b5a6db0a677841027db2e69668e2f82aad2
laymond1 <costor107@gmail.com>
2023-03-12 02:51:24 +0900

    📎 casia experiment test files updated

--- examples/casia_test_base.py
+++ examples/casia_test_base.py
@@ -7,26 +7,22 @@ from clhive.loggers import BaseLogger, Logger
 from clhive.data import SplitCIFAR10, SplitCIFAR100, RepresentationCIFAR10, CASIAWebDataset, LFWPairDataset
 from clhive.data import CASIAWebDataset, LFWPairDataset, CALFWPairDataset, CPLFWPairDataset, AGEDB30PairDataset
 from clhive.utils.evaluators import ContinualEvaluator, ProbeEvaluator, RepresentationEvaluator
-from clhive.utils.generic import seedEverything, adjust_learning_rate, warmup_learning_rate, TwoCropTransform
+from clhive.utils.generic import seedEverything, warmup_learning_rate, TwoCropTransform
 from clhive.scenarios import ClassIncremental, TaskIncremental, RepresentationIncremental
 from clhive.models import ContinualModel, ContinualAngularModel
 from clhive.methods import auto_method
 from clhive import Trainer, SupConTrainer, ReplayBuffer
 
-# import os
-# os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
-
-DEFAULT_RANDOM_SEED = 2023
-seedEverything(DEFAULT_RANDOM_SEED)
-
 
 def parse_option():
     parser = argparse.ArgumentParser('argument for base method training')
 
+    parser.add_argument('--seed', type=int, default=2023,
+                        help='set seed')
     parser.add_argument('--batch_size', type=int, default=128,
                         help='batch_size')
+    parser.add_argument('--test_batch_size', type=int, default=512,
+                        help='test_batch_size')
     parser.add_argument('--num_workers', type=int, default=0,
                         help='num of workers to use')
     parser.add_argument('--n_tasks', type=int, default=5,
@@ -35,7 +31,7 @@ def parse_option():
                         help='number of training epochs')
 
     # optimization
-    parser.add_argument('--learning_rate', type=float, default=0.01,
+    parser.add_argument('--learning_rate', type=float, default=0.1,
                         help='learning rate')
     parser.add_argument('--lr_decay_epochs', type=str, default='4,8',
                         help='where to decay lr, can be a list')
@@ -58,13 +54,13 @@ def parse_option():
     parser.add_argument('--cl_method', type=str, default='finetuning',
                         choices=['finetuning', 'er', 'der', 'lwf', 'ewc', 'crl'], help='choose continual learning method')
     parser.add_argument('--buffer_capacity', type=int, default=50*10, help='buffer_capacity')
-    parser.add_argument('--backbone_name', type=str, default='resnet50',
+    parser.add_argument('--backbone_name', type=str, default='iresnet50',
                         choices=['resnet18', 'resnet50', 'iresnet50', None], help='choose backbone_name')
     parser.add_argument('--rep_method', type=str, default='linear',
                         choices=['linear', 'arcface', 'cosface', 'sphereface', 'supconmlp'], help='choose representation learning method (head_name)')
     
     # temperature
-    parser.add_argument('--temp', type=float, default=0.07,
+    parser.add_argument('--temp', type=float, default=0.1,
                         help='temperature for loss function')
 
     # other setting
@@ -112,53 +108,47 @@ else:
 
 def main(opt):
 
-    # HParams
-    batch_size = opt.batch_size
-    n_tasks = opt.n_tasks
-    n_epochs = opt.n_epochs
-    buffer_capacity = opt.buffer_capacity
-    backbone_name = opt.backbone_name
-    head_name = opt.rep_method
-    cl_method = opt.cl_method
+    DEFAULT_RANDOM_SEED = opt.seed
+    seedEverything(DEFAULT_RANDOM_SEED)
 
     scenario = ClassIncremental(
-        dataset=dataset, n_tasks=n_tasks, batch_size=batch_size, n_workers=0
+        dataset=dataset, n_tasks=opt.n_tasks, batch_size=opt.batch_size, n_workers=0
     )
 
     print(f"Number of tasks: {scenario.n_tasks} | Number of classes: {scenario.n_classes}")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Model
-    model = ContinualModel.auto_model(backbone_name=backbone_name, scenario=scenario, head_name=head_name).to(device)
+    model = ContinualModel.auto_model(backbone_name=opt.backbone_name, scenario=scenario, head_name=opt.rep_method).to(device)
 
-    buffer = ReplayBuffer(capacity=buffer_capacity, device=device)
+    buffer = ReplayBuffer(capacity=opt.buffer_capacity, device=device)
     # Replay buffer and ER agent
     agent = auto_method(
-        name=cl_method,
+        name=opt.cl_method,
         model=model,
-        optim=SGD(model.parameters(), lr=0.05, momentum=0.9, weight_decay=1e-4),
+        optim=SGD(model.parameters(), lr=opt.learning_rate, momentum=opt.momentum, weight_decay=opt.weight_decay),
         buffer=buffer,
     )
 
     # Representation Evaluator: n_tasks arguments 필요 없음.
     # LFW Test Dataset
     lfw_test_scenario = RepresentationIncremental(
-        lfw_test_dataset, n_tasks=n_tasks, batch_size=batch_size, n_workers=0
+        lfw_test_dataset, n_tasks=opt.n_tasks, batch_size=opt.test_batch_size, n_workers=0
     )
     lfw_rep_evaluator = RepresentationEvaluator(method=agent, eval_scenario=lfw_test_scenario, device=device)
     # CALFW Test Dataset
     calfw_test_scenario = RepresentationIncremental(
-        calfw_test_dataset, n_tasks=n_tasks, batch_size=batch_size, n_workers=0
+        calfw_test_dataset, n_tasks=opt.n_tasks, batch_size=opt.test_batch_size, n_workers=0
     )
     calfw_rep_evaluator = RepresentationEvaluator(method=agent, eval_scenario=calfw_test_scenario, device=device)
     # CPLFW Test Dataset
     cplfw_test_scenario = RepresentationIncremental(
-        cplfw_test_dataset, n_tasks=n_tasks, batch_size=batch_size, n_workers=0
+        cplfw_test_dataset, n_tasks=opt.n_tasks, batch_size=opt.test_batch_size, n_workers=0
     )
     cplfw_rep_evaluator = RepresentationEvaluator(method=agent, eval_scenario=cplfw_test_scenario, device=device)
     # AGEDB30 Test Dataset
     agedb_30_test_scenario = RepresentationIncremental(
-        agedb_30_test_dataset, n_tasks=n_tasks, batch_size=batch_size, n_workers=0
+        agedb_30_test_dataset, n_tasks=opt.n_tasks, batch_size=opt.test_batch_size, n_workers=0
     )
     agedb_30_rep_evaluator = RepresentationEvaluator(method=agent, eval_scenario=agedb_30_test_scenario, device=device)
 
@@ -166,8 +156,8 @@ def main(opt):
 
     # Logger 
     # TODO 반복 실험을 한다면 "5_runs" directory를 추가하여 실험 저장
-    opt.save_path = './save/{}/{}_steps_{}_{}'.format(
-            opt.dataset, opt.n_tasks, opt.cl_method, opt.rep_method
+    opt.save_path = './save/{}/{}_seed/{}_steps_{}_{}'.format(
+            opt.dataset, opt.seed, opt.n_tasks, opt.cl_method, opt.rep_method
             )
     logger = Logger(opt.n_tasks)
     logger.open_txt(opt.save_path)
@@ -175,7 +165,7 @@ def main(opt):
 
     # Trainer
     trainer = Trainer(
-        opt=opt, method=agent, scenario=scenario, evaluator=evaluators, n_epochs=n_epochs, device=device, logger=logger
+        opt=opt, method=agent, scenario=scenario, evaluator=evaluators, n_epochs=opt.n_epochs, device=device, logger=logger
     )
     trainer.fit()
 
